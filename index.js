34cf7f9b9e655cf9ee390f6b0f20348da3da5bf2
Charith Wickramasinghe <131009269+charith-codex@users.noreply.github.com>
2024-01-03 19:24:55 +0530

    ver 1.0

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+function initializeSlider() {
+
+}
+function showSlide(index) {
+
+}
+function prevSlide() {
+
+}
+function nextSlide() {
+
+}
