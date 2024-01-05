0deddb50a9a90eb180a2dcec672c7c8ff512386c
Charith Wickramasinghe <131009269+charith-codex@users.noreply.github.com>
2024-01-05 11:55:51 +0530

    ver 1.3

--- index.js
+++ index.js
@@ -6,19 +6,28 @@ let intervalId = null;
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
 function initializeSlider() {
-    if(slides.length > 0){
+    if (slides.length > 0) {
         slides[slideIndex].classList.add("displaySlide");
         intervalId = setInterval(nextSlide, 5000);
     }
 }
 function showSlide(index) {
+
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
     slides.forEach(slide => {
         slide.classList.remove("displaySlide");
     });
     slides[slideIndex].classList.add("displaySlide");
 }
 function prevSlide() {
-
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
 }
 function nextSlide() {
     slideIndex++;
